ae325bc5a03b1159f2125f7d0da7520e1bad29ec
v.efremov <v.efremov@cian.ru>
2024-02-08 19:16:18 +0700

    remove redundant code

--- sort_service.py
+++ sort_service.py
@@ -3,11 +3,3 @@ from product import Product
 
 def sort_products_by_price(products: list[Product]):
     return sorted(products, key=lambda product: product.price)
-
-
-if __name__ == '__main__':
-    products = [
-        Product(id=1, name='apple', price=10),
-        Product(id=2, name='potato', price=2.1),
-    ]
-    print(sort_products_by_price(products))
